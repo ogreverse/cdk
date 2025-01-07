6b68558083b1709ab15805418e39b73981d34df0
ogreverse <glado.o.guild@gmail.com>
2025-01-07 21:30:40 +0900

    add: initialize simple_rag_with_bedrock

--- simple_rag_with_bedrock/README.md
+++ simple_rag_with_bedrock/README.md
@@ -3,3 +3,12 @@
 ## Concept
 
 Bedrockを使った簡易なRAG環境の構築
+
+## Commands
+
+* `npm run build`   compile typescript to js
+* `npm run watch`   watch for changes and compile
+* `npm run test`    perform the jest unit tests
+* `npx cdk deploy`  deploy this stack to your default AWS account/region
+* `npx cdk diff`    compare deployed stack with current state
+* `npx cdk synth`   emits the synthesized CloudFormation template
---
+++ simple_rag_with_bedrock/test/simple_rag_with_bedrock.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SimpleRagWithBedrock from '../lib/simple_rag_with_bedrock-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/simple_rag_with_bedrock-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SimpleRagWithBedrock.SimpleRagWithBedrockStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
